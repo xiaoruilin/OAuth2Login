0fec96f9c3698c252b0fe8583ab730eaf6aa2198
lepipele <lepipele@users.noreply.github.com>
2015-03-03 17:32:34 -0600

    GoogleUserData refactoring and testing

--- Oauth2Login/Service/GoogleService.cs
+++ Oauth2Login/Service/GoogleService.cs
@@ -66,7 +66,7 @@ namespace Oauth2Login.Service
 
         public string id { get; set; }
         public string email { get; set; }
-        public string verified_email { get; set; }
+        public bool verified_email { get; set; }
         public string name { get; set; }
         public string given_name { get; set; }
         public string family_name { get; set; }
